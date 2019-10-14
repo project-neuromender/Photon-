b1dc76562aa42c4fd6bb605aee912546a1680c36
nuramiera <nuramiera@users.github.com>
2019-10-14 14:11:45 +0800

    Update VR Manifest
    - update for my pc

--- Project/Assets/Interface/Script/UI/Rooms/PlayerListing.cs
+++ Project/Assets/Interface/Script/UI/Rooms/PlayerListing.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Photon.Realtime;
+using Photon.Pun;
 using UnityEngine.UI;
+using Photon.Realtime;
 
 public class PlayerListing : MonoBehaviour
 {
@@ -10,12 +11,18 @@ public class PlayerListing : MonoBehaviour
     private Text _text;
 
     public Player Player { get; private set; }
-
-
+    public PlayerInputField PlayerInputField { get; private set; }
 
     public void SetPlayerInfo(Player player)
     {
         Player = player;
         _text.text = player.NickName;
     }
+
+    public void SetPlayerInfo1(PlayerInputField player)
+    {
+        PlayerInputField = player;
+        _text.text = PhotonNetwork.NickName;
+    }
 }
+
