7c733d3807a77f2ff4a4f7f4c0fdd6e0f2e40835
syazwanianuar <592680@murdoch.edu.au>
2019-10-15 15:44:50 +0800

    Latest (15/10)

--- Project/Assets/IKEA/VR-Socket-master/Assets/Script/Interaction/Rotation.cs
+++ Project/Assets/IKEA/VR-Socket-master/Assets/Script/Interaction/Rotation.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-    public float moveSpeed;
+    GameObject target;
+
+     void Start()
+    {
+        target= GameObject.Find("Table plane");
+    }
+   
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z); 
+    }
+
+    /*public float moveSpeed;
 
     void Update()
     {
         float v = Input.GetAxis("Vertical") * moveSpeed;
         transform.Translate(new Vector3(0, 0, v) * Time.deltaTime);
-    }
+    }*/
 }
