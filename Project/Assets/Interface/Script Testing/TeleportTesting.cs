9f9d2cec30ba7da438ad82c0cd760366fba3eca3
nuramiera <nuramiera@users.github.com>
2019-10-16 16:54:11 +0800

    Testing Teleport

--- Project/Assets/Interface/Script Testing/TeleportTesting.cs
+++ Project/Assets/Interface/Script Testing/TeleportTesting.cs
@@ -5,17 +5,27 @@ using UnityEngine.SceneManagement;
 using Valve.VR;
 using Valve.VR.Extras;
 using Valve.VR.InteractionSystem;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class TeleportTesting : MonoBehaviour
+public class TeleportTesting : MonoBehaviourPunCallbacks
 {
     public SteamVR_LaserPointer laserPointer;
     public GameObject player;
     public Transform Teleport_Location;
     public Transform Teleport_Location1;
 
+    private SteamVR_Behaviour_Pose pose = null;
+    public SteamVR_Action_Boolean Teleport;
+
+
     void Awake()
-    {
-        laserPointer.PointerClick += PointerClick;
+    {        
+        if (photonView.IsMine)
+        {
+            laserPointer.PointerClick += PointerClick;
+            pose = GetComponent<SteamVR_Behaviour_Pose>();
+        }        
     }
 
     public void PointerClick(object sender, PointerEventArgs e)
@@ -27,17 +37,38 @@ public class TeleportTesting : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (photonView.IsMine)
+        {
+            if (Teleport.GetLastStateUp(pose.inputSource))
+            {
+                TeleportRunning();
+            }
+        }
+    }
+
     public void Button()
     {
         //Debug.Log("Button was clicked");
+        PhotonNetwork.LeaveRoom();
         SceneManager.LoadScene("Rooms");
+    }
+
+    void TeleportRunning()
+    {
+        if ((gameObject.tag == "Player"))
+        {
+            player.transform.position = Teleport_Location.transform.position;
+        }
+        Debug.Log("Teleport Running");
     }
 
     /*public void Teleport(Collider other)
     {
         if ((other.gameObject.tag = "Player") && Input.GetKeyDown(KeyCode.E))
         { }
-    }*/
+    }
 
     void OnTriggerStay(Collider other)
     {
@@ -45,5 +76,5 @@ public class TeleportTesting : MonoBehaviour
         {
             player.transform.position = Teleport_Location.transform.position;
         }
-    }
+    }*/
 }
